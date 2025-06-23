077695f1c4a5912454b8928619a7f579a5e29beb
Yohai Bar Sinai <6164157+yohai@users.noreply.github.com>
2025-06-23 15:58:08 +0300

    factor exponential too

---
+++ assets/js/exp_interpolation_plot.js
@@ -0,0 +1,100 @@
+// complex plane under f(z)=exp(z)
+const xScaleExp = d3.scaleLinear().domain([-4.5, 4.5]).range([0, width]);
+const yScaleExp = d3.scaleLinear().domain([-4.5, 4.5]).range([height, 0]);
+
+const svgExp = d3.select("#expchart");
+
+//Add Axes
+svgExp.append("g")
+    .attr("transform", `translate(0,${height / 2})`)
+    .call(d3.axisBottom(xScaleExp).tickValues([-Math.PI, Math.PI]).tickFormat(d => {
+        if (d === -Math.PI) return "-π";
+        if (d === Math.PI) return "π";
+        return d;
+    }))
+    .attr("stroke-width", 2)
+    .selectAll("text")
+    .style("font-size", "20px");
+
+svgExp.append("g")
+    .attr("transform", `translate(${width / 2},0)`)
+    .call(d3.axisLeft(yScaleExp).tickValues([-Math.PI, Math.PI]).tickFormat(d => {
+        if (d === -Math.PI) return "-π";
+        if (d === Math.PI) return "π";
+        return d;
+    }))
+    .attr("stroke-width", 2)
+    .selectAll("text")
+    .style("font-size", "20px");
+
+function genLinesExp() {
+    const hlinesExp = [];
+    const vlinesExp = [];
+    const coarseExp = Math.PI/10;
+    // Horizontal lines (each line is a set of fixed y with varying x)
+    for (let y = -Math.PI; y <= Math.PI; y += coarseExp) {
+        const hline = [];
+        for (let x = -Math.PI; x <= Math.PI; x += fine) {
+            hline.push([x, y]);
+        }
+        hlinesExp.push(hline);
+    }
+
+    // Vertical lines (each line is a set of fixed x with varying y)
+    for (let x = -Math.PI; x <= Math.PI; x += coarseExp) {
+        const vline = [];
+        for (let y = -Math.PI; y <= Math.PI; y += fine) {
+            vline.push([x, y]);
+        }
+        vlinesExp.push(vline);
+    }
+
+    return { hlinesExp, vlinesExp };
+}
+
+
+
+function fexp(points, t) {
+    return points.map((p, _) => [
+        t * Math.exp(p[0]) * Math.cos(p[1]) + (1-t) * p[0],
+        t * Math.exp(p[0]) * Math.sin(p[1]) + (1-t) * p[1],
+    ]);
+}
+
+
+const { hlinesExp, vlinesExp } = genLinesExp();
+function updateExp(t) {
+    
+    const hLinesMapped = hlinesExp.map(line => fexp(line, t));
+    const vLinesMapped = vlinesExp.map(line => fexp(line, t));
+
+    drawLines(hLinesMapped, "blue");
+    drawLines(vLinesMapped, "red");
+    
+    svgExp.selectAll("g").raise();
+}
+function drawLines(lines, color) {
+    const lineGenerator = d3.line()
+        .x(d => xScale(d[0]))
+        .y(d => yScale(d[1]));
+
+    svgExp.selectAll(`.line-${color}`)
+        .data(lines)
+        .join("path")
+        .attr("class", `line-${color}`)
+        .attr("d", lineGenerator)
+        .attr("fill", "none")
+        .attr("stroke", color)
+        .attr("stroke-width", 1);
+}
+updateExp(0.0);
+
+const tInput = d3.select("#t");
+const tValue = d3.select("#tValue");
+
+function handleInputExp() {
+    const t = +tInput.node().value;
+    tValue.text(t.toFixed(1));
+    updateExp(t);
+}
+tInput.on("input", handleInputExp);
